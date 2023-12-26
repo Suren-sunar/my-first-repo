a3b860a9c52daf0a6fa9eb4793936b4700fb7426
suren-sunar <surendra.sunar480@gmail.com>
2023-12-26 22:10:33 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const broadway = document.querySelector(".way");
+broadway.addEventListener('click', () => {
+    var userInput = prompt('Hello ! Enter your class number');
+    console.log('your class is',userInput);
+});
